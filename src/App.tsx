69cc02969fcd603936e14f6455578182638bf040
横山　一希 <codetrain.kazuki.yokoyama@gmail.com>
2025-02-01 12:49:11 +0900

    コンポーネント課題トップページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Component from "./pages/Component";
+import ButtonComponent from "./pages/ButtonComponent";
 
 function App() {
   return (
@@ -9,12 +11,15 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/component">コンポーネント</Link></li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/component" element={<Component />} />
+        <Route path="/component/button" element={<ButtonComponent />} />
       </Routes>
     </Router>
   );
