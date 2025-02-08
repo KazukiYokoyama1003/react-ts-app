a1f760da2b5bd670542c4d99c8a5439d5c5a22f7
Kazuki Yokoyama <kazuki_yokoyama@respawn.jp>
2025-02-08 19:49:42 +0900

    ツールチップコンポーネントの作成

---
+++ src/pages/TooltipComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Tooltip from "../component/tooltip/Tooltip";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Tooltip />
+    </div>
+  );
+};
+
+export default App;
