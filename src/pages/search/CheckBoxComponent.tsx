773e917ef831cbb7983348f6eed0fae0871d5a12
Kazuki Yokoyama <kazuki_yokoyama@respawn.jp>
2025-03-02 16:55:43 +0900

    チェックボックスのエラー解消

--- src/pages/search/CheckBoxComponent.tsx
+++ src/pages/search/CheckBoxComponent.tsx
@@ -1,13 +1,18 @@
 import CheckBox from "../../component/checkbox/CheckBox";
 
-function App() {
-  return (
-    <div style={{ display: "flex", gap: "10px", padding: "20px" }}>
-      <CheckBox color="blue" label="ボックス1"/>
-      <CheckBox color="blue" label="ボックス2"/>
-      <CheckBox color="blue" label="ボックス3"/>
-    </div>
-  );
+interface CheckBoxComponentProps {
+  values: string[];
+  setValues: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-export default App;
+const CheckBoxComponent = ({ values, setValues }: CheckBoxComponentProps) => {
+  return (
+    <div style={{ display: "flex", gap: "10px", padding: "20px" }}>
+      <CheckBox color="blue" label="ボックス1" values={values} setValues={setValues} />
+      <CheckBox color="blue" label="ボックス2" values={values} setValues={setValues} />
+      <CheckBox color="blue" label="ボックス3" values={values} setValues={setValues} />
+    </div>
+  );
+};
+
+export default CheckBoxComponent;
